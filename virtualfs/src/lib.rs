88d00cbd5474eb203aed25b59d94573648df7af3
Al Liu <scygliu1@gmail.com>
2024-04-28 02:08:13 +0800

    Init project

---
+++ virtualfs/src/lib.rs
@@ -0,0 +1,6 @@
+//! Virtual File System
+#![allow(clippy::type_complexity)]
+#![deny(warnings, missing_docs)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, allow(unused_attributes))]
+#![cfg_attr(not(all(feature = "std", test)), no_std)]
