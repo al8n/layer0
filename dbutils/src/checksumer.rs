0d75934dad48776862a57eff3e355ce27ca0fc22
Al Liu <scygliu1@gmail.com>
2024-09-16 03:47:37 +0800

    Optimize checksumer feature gate

--- dbutils/src/checksumer.rs
+++ dbutils/src/checksumer.rs
@@ -21,30 +21,39 @@ pub trait Checksumer {
 pub struct Crc32(crc32fast::Hasher);
 
 #[cfg(feature = "crc32fast")]
-impl Checksumer for Crc32 {
-  #[inline]
-  fn checksum(&self, buf: &[u8]) -> u64 {
-    crc32fast::hash(buf) as u64
+const _: () = {
+  impl Crc32 {
+    /// Create a new CRC32 checksumer.
+    #[inline]
+    pub fn new() -> Self {
+      Self(crc32fast::Hasher::new())
+    }
   }
 
-  #[inline]
-  fn update(&mut self, buf: &[u8]) {
-    self.0.update(buf)
+  impl Checksumer for Crc32 {
+    #[inline]
+    fn checksum(&self, buf: &[u8]) -> u64 {
+      crc32fast::hash(buf) as u64
+    }
+
+    #[inline]
+    fn update(&mut self, buf: &[u8]) {
+      self.0.update(buf)
+    }
+
+    #[inline]
+    fn reset(&mut self) {
+      self.0 = crc32fast::Hasher::new()
+    }
+
+    #[inline]
+    fn digest(&self) -> u64 {
+      self.0.clone().finalize() as u64
+    }
   }
 
-  #[inline]
-  fn reset(&mut self) {
-    self.0 = crc32fast::Hasher::new()
-  }
-
-  #[inline]
-  fn digest(&self) -> u64 {
-    self.0.clone().finalize() as u64
-  }
-}
-
-#[cfg(feature = "crc32fast")]
-impl super::CheapClone for Crc32 {}
+  impl super::CheapClone for Crc32 {}
+};
 
 /// XxHash checksumer.
 #[cfg(feature = "xxhash64")]
@@ -56,51 +65,51 @@ pub struct XxHash64 {
 }
 
 #[cfg(feature = "xxhash64")]
-impl XxHash64 {
-  /// Create a new XxHash64 with seed 0.
-  #[inline]
-  pub const fn new() -> Self {
-    Self {
-      seed: 0,
-      hasher: xxhash_rust::xxh64::Xxh64::new(0),
+const _: () = {
+  impl XxHash64 {
+    /// Create a new XxHash64 with seed 0.
+    #[inline]
+    pub const fn new() -> Self {
+      Self {
+        seed: 0,
+        hasher: xxhash_rust::xxh64::Xxh64::new(0),
+      }
+    }
+
+    /// Create a new XxHash64 with a seed.
+    #[inline]
+    pub const fn with_seed(seed: u64) -> Self {
+      Self {
+        seed,
+        hasher: xxhash_rust::xxh64::Xxh64::new(seed),
+      }
     }
   }
 
-  /// Create a new XxHash64 with a seed.
-  #[inline]
-  pub const fn with_seed(seed: u64) -> Self {
-    Self {
-      seed,
-      hasher: xxhash_rust::xxh64::Xxh64::new(seed),
+  impl Checksumer for XxHash64 {
+    #[inline]
+    fn checksum(&self, buf: &[u8]) -> u64 {
+      xxhash_rust::xxh64::xxh64(buf, self.seed)
+    }
+
+    #[inline]
+    fn reset(&mut self) {
+      self.hasher.reset(self.seed)
+    }
+
+    #[inline]
+    fn update(&mut self, buf: &[u8]) {
+      self.hasher.update(buf)
+    }
+
+    #[inline]
+    fn digest(&self) -> u64 {
+      self.hasher.digest()
     }
   }
-}
 
-#[cfg(feature = "xxhash64")]
-impl Checksumer for XxHash64 {
-  #[inline]
-  fn checksum(&self, buf: &[u8]) -> u64 {
-    xxhash_rust::xxh64::xxh64(buf, self.seed)
-  }
-
-  #[inline]
-  fn reset(&mut self) {
-    self.hasher.reset(self.seed)
-  }
-
-  #[inline]
-  fn update(&mut self, buf: &[u8]) {
-    self.hasher.update(buf)
-  }
-
-  #[inline]
-  fn digest(&self) -> u64 {
-    self.hasher.digest()
-  }
-}
-
-#[cfg(feature = "xxhash64")]
-impl super::CheapClone for XxHash64 {}
+  impl super::CheapClone for XxHash64 {}
+};
 
 /// XxHash64 (with xxh3 support) checksumer.
 #[cfg(feature = "xxhash3")]
@@ -112,48 +121,48 @@ pub struct XxHash3 {
 }
 
 #[cfg(feature = "xxhash3")]
-impl XxHash3 {
-  /// Create a new XxHash64 with seed 0.
-  #[inline]
-  pub const fn new() -> Self {
-    Self {
-      seed: 0,
-      hasher: xxhash_rust::xxh3::Xxh3::new(),
+const _: () = {
+  impl XxHash3 {
+    /// Create a new XxHash64 with seed 0.
+    #[inline]
+    pub const fn new() -> Self {
+      Self {
+        seed: 0,
+        hasher: xxhash_rust::xxh3::Xxh3::new(),
+      }
+    }
+
+    /// Create a new XxHash64 with a seed.
+    #[inline]
+    pub fn with_seed(seed: u64) -> Self {
+      Self {
+        seed,
+        hasher: xxhash_rust::xxh3::Xxh3::with_seed(seed),
+      }
     }
   }
 
-  /// Create a new XxHash64 with a seed.
-  #[inline]
-  pub fn with_seed(seed: u64) -> Self {
-    Self {
-      seed,
-      hasher: xxhash_rust::xxh3::Xxh3::with_seed(seed),
+  impl Checksumer for XxHash3 {
+    #[inline]
+    fn checksum(&self, buf: &[u8]) -> u64 {
+      xxhash_rust::xxh3::xxh3_64_with_seed(buf, self.seed)
+    }
+
+    #[inline]
+    fn update(&mut self, buf: &[u8]) {
+      self.hasher.update(buf)
+    }
+
+    #[inline]
+    fn reset(&mut self) {
+      self.hasher.reset()
+    }
+
+    #[inline]
+    fn digest(&self) -> u64 {
+      self.hasher.digest()
     }
   }
-}
 
-#[cfg(feature = "xxhash3")]
-impl Checksumer for XxHash3 {
-  #[inline]
-  fn checksum(&self, buf: &[u8]) -> u64 {
-    xxhash_rust::xxh3::xxh3_64_with_seed(buf, self.seed)
-  }
-
-  #[inline]
-  fn update(&mut self, buf: &[u8]) {
-    self.hasher.update(buf)
-  }
-
-  #[inline]
-  fn reset(&mut self) {
-    self.hasher.reset()
-  }
-
-  #[inline]
-  fn digest(&self) -> u64 {
-    self.hasher.digest()
-  }
-}
-
-#[cfg(feature = "xxhash3")]
-impl super::CheapClone for XxHash3 {}
+  impl super::CheapClone for XxHash3 {}
+};
