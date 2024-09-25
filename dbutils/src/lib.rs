117e73cfb9ed1ca4d83a7e4c8e31ee24c551f017
al8n <scygliu1@gmail.com>
2024-09-25 22:11:27 +0800

    Add `builder` macro

--- dbutils/src/lib.rs
+++ dbutils/src/lib.rs
@@ -27,3 +27,78 @@ pub use traits::Comparator;
 pub use equivalent;
 
 pub use cheap_clone::CheapClone;
+
+#[doc(hidden)]
+pub mod __private {
+  pub use paste;
+}
+
+/// A macro to generate builder types.
+///
+/// A builder type is typically has a size and a closure that can fill a buffer with the given size.
+///
+/// ## Example
+///
+/// ```rust
+/// use dbutils::{builder, buffer::VacantBuffer};
+///
+/// // Generates a builder type named `ValueBuilder` with a maximum size of `u32`.
+/// builder!(
+///   /// A builder for a value
+///   ValueBuilder(u32)
+/// );
+///
+/// let builder = ValueBuilder::new(16, |mut buf: VacantBuffer<'_>| {
+///   buf.fill(1);
+///   Ok::<(), core::convert::Infallible>(())
+/// });
+///
+/// assert_eq!(builder.size(), 16);
+/// ```
+#[macro_export]
+macro_rules! builder {
+  ($(
+    $(#[$meta:meta])*
+    $wrapper_vis:vis $name:ident($vis:vis $size:ident));+ $(;)?
+  ) => {
+    $(
+      $crate::__private::paste::paste! {
+        $(#[$meta])*
+        #[derive(::core::marker::Copy, ::core::clone::Clone, ::core::fmt::Debug)]
+        $wrapper_vis struct $name <F> {
+          /// The required size of the builder.
+          $vis size: $size,
+          /// The builder closure.
+          $vis f: F,
+        }
+
+        impl<F> $name<F> {
+          #[doc = "Creates a new `" $name "` with the given size and builder closure."]
+          #[inline]
+          pub const fn new(size: $size, f: F) -> Self
+          {
+            Self { size, f }
+          }
+
+          #[doc = "Returns the required `" $name "` size."]
+          #[inline]
+          pub const fn size(&self) -> $size {
+            self.size
+          }
+
+          #[doc = "Returns the `" $name "` builder closure."]
+          #[inline]
+          pub const fn builder(&self) -> &F {
+            &self.f
+          }
+
+          /// Deconstructs the value builder into the size and the builder closure.
+          #[inline]
+          pub fn into_components(self) -> ($size, F) {
+            (self.size, self.f)
+          }
+        }
+      }
+    )*
+  };
+}
