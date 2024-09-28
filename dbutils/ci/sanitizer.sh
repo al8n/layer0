472e8babb9ad567724090da456b6212dd254b22d
al8n <scygliu1@gmail.com>
2024-09-28 17:46:46 +0800

    a

--- dbutils/ci/sanitizer.sh
+++ dbutils/ci/sanitizer.sh
@@ -4,18 +4,17 @@ set -ex
 
 export ASAN_OPTIONS="detect_odr_violation=0 detect_leaks=0"
 
-# Run address sanitizer with cargo-hack
+# Run address sanitizer
+cargo clean
 RUSTFLAGS="-Z sanitizer=address" \
 cargo test -p dbutils
 
-# Run leak sanitizer with cargo-hack
+# Run leak sanitizer
+cargo clean
 RUSTFLAGS="-Z sanitizer=leak" \
 cargo test -p dbutils
 
+cargo clean
 RUSTFLAGS="--cfg all_tests -Zsanitizer=memory -Zsanitizer-memory-track-origins" \
 RUSTDOCFLAGS="-Zsanitizer=memory -Zsanitizer-memory-track-origins" \
 cargo test -Zbuild-std --release --tests --target x86_64-unknown-linux-gnu -p dbutils
-
-# Run thread sanitizer with cargo-hack
-RUSTFLAGS="-Z sanitizer=thread" \
-cargo -Zbuild-std test -p dbutils
