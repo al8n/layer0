177784a15744b4615146a4dbcc11d07353b1cf52
al8n <scygliu1@gmail.com>
2025-02-17 00:28:35 +0800

    Update `dbutils` to `0.14.1`

--- dbutils/src/leb128.rs
+++ dbutils/src/leb128.rs
@@ -1,8 +1,12 @@
 pub use const_varint::{
-  decode_i128_varint, decode_i16_varint, decode_i32_varint, decode_i64_varint, decode_u128_varint,
-  decode_u16_varint, decode_u32_varint, decode_u64_varint, encode_i128_varint, encode_i16_varint,
-  encode_i32_varint, encode_i64_varint, encode_u128_varint, encode_u16_varint, encode_u32_varint,
-  encode_u64_varint, encoded_i128_varint_len, encoded_i16_varint_len, encoded_i32_varint_len,
-  encoded_i64_varint_len, encoded_u128_varint_len, encoded_u16_varint_len, encoded_u32_varint_len,
-  encoded_u64_varint_len, DecodeError as DecodeVarintError, EncodeError, Varint,
+  decode_duration, decode_i128_varint, decode_i16_varint, decode_i32_varint, decode_i64_varint,
+  decode_u128_varint, decode_u16_varint, decode_u32_varint, decode_u64_varint, encode_duration,
+  encode_duration_to, encode_i128_varint, encode_i128_varint_to, encode_i16_varint,
+  encode_i16_varint_to, encode_i32_varint, encode_i32_varint_to, encode_i64_varint,
+  encode_i64_varint_to, encode_u128_varint, encode_u128_varint_to, encode_u16_varint,
+  encode_u16_varint_to, encode_u32_varint, encode_u32_varint_to, encode_u64_varint,
+  encode_u64_varint_to, encoded_duration_len, encoded_i128_varint_len, encoded_i16_varint_len,
+  encoded_i32_varint_len, encoded_i64_varint_len, encoded_u128_varint_len, encoded_u16_varint_len,
+  encoded_u32_varint_len, encoded_u64_varint_len, DecodeError as DecodeVarintError, EncodeError,
+  Varint,
 };
